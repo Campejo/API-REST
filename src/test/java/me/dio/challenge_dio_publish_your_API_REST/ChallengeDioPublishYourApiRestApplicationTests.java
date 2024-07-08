fd187d1b8d2ffc0450c5a8667c5c74a36233f239
Thiago Campello <campellothiago@hotmail.com>
2024-07-08 20:03:16 -0300

    Initial commit

---
+++ src/test/java/me/dio/challenge_dio_publish_your_API_REST/ChallengeDioPublishYourApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.challenge_dio_publish_your_API_REST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeDioPublishYourApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
